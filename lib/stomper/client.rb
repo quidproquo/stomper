d5b6c439b74b8b70864c9c6358acf157fe352c3d
Ilya Scharrenbroich <$EMAIL>
2012-12-02 19:25:25 -0500

    adding publishing

--- lib/stomper/client.rb
+++ lib/stomper/client.rb
@@ -4,8 +4,8 @@ class Stomper::Client
 
   @@connection = nil
 
-  def self.publish
-    connection
+  def self.publish(queue, message)
+    connection.publish(queue, message)
   end
 
   protected
--- spec/lib/stomper/client_spec.rb
+++ spec/lib/stomper/client_spec.rb
@@ -2,17 +2,29 @@ require 'spec_helper'
 
 describe Stomper::Client do
 
-  it { should be }
+  let(:connection) { double(:connection) }
+  subject { Stomper::Client }
+
+  before {
+    Stomp::Connection.stub(:new).and_return(connection)
+  }
 
   describe :class_methods do
-
     describe :connection do
-      let(:connection) { double(:connection) }
       subject { Stomper::Client.connection }
-      before {
-        Stomp::Connection.stub(:new).and_return(connection)
-      }
       it { should_not be_nil }
+    end
+
+    describe :publish do
+      let(:queue) { '/queue/test' }
+      let(:message) { 'Test message' }
+      before {
+        connection.should_receive(:publish).with(queue, message)
+      }
+      it 'should publish the message' do
+        subject.publish(queue, message)
+      end
+
     end
 
   end
